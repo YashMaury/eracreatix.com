9d2a834d78b6490d593c56bbb034529a7e6ef4f0
Bal anand <giplanand@gmail.com>
2024-04-27 17:34:17 +0530

    final

--- include/config.php
+++ include/config.php
@@ -10,4 +10,15 @@ if (mysqli_connect_errno())
 {
  echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
+
+// define('DB_SERVER','localhost');
+// define('DB_USER','glintqnj_shopping');
+// define('DB_PASS' ,'12qw!@QW9984');
+// define('DB_NAME', 'glintqnj_shopping');
+// $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+// // Check connection
+// if (mysqli_connect_errno())
+// {
+//  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+// }
 ?>
